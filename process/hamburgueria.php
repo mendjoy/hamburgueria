a5b4de1912f462b960fd0c684fb8202921b85f00
joyce <joycemendes.jm79@gmail.com>
2023-02-05 18:25:47 -0300

    resgatando dados do banco de dados para o form

--- process/hamburgueria.php
+++ process/hamburgueria.php
@@ -1,6 +1,56 @@
 <?php
 
-    include_once("conn.php");
+ include_once("conn.php");
 
+ $method = $_SERVER["REQUEST_METHOD"];
+
+ if($method === "GET"){
+
+    $paesQuery = $conn->query("SELECT * FROM pao;");
+    $paes = $paesQuery->fetchAll();
+
+    $burguersQuery = $conn->query("SELECT * FROM burguer;");
+    $burguers = $burguersQuery->fetchAll();
+
+    $acompanhamentosQuery = $conn->query("SELECT * FROM acompanhamentos");
+    $acompanhamentos = $acompanhamentosQuery->fetchAll();
+
+
+
+
+ } else if($method === "POST"){
+
+    $data = $_POST;
+
+    $pao = $data["pao"];
+    $burguer = $data["burguer"];
+    $acompanhamento = $data["acompanhamento"];
+
+    if(count($acompanhamentos) > 4 ){
+
+        $_SESSION["msg"] = "Selecione no máximo 4 acompanhamentos";
+        $_SESSION["status"] = "warning";
+
+    } else {
+
+        //salvando dados 
+        $stmt = $conn->prepare("INSERT INTO hamburguers (burguer_id, pao_id) VALUES (:burguer, :pao);");
+
+        //filtrando inputs 
+        $stmt = bindParam(":burguer", $burguer, PDO::PARAM_INT);
+        $stmt = bindParam(":pao", $pao, PDO::PARAM_INT);
+
+        //executar a query 
+        $stmt->execute();
+
+        $hamburguerId = $conn->lasInsertId();
+
+        $stmt = $conn->prepare("INSERT INTO hamburguer_acompanhamentos (hamburguers_id, acompanhamentos_id VALUES (:, :acompanhamentos)");
+    }
+
+ }
+
+ //retorna para pagina inicial 
+ 
    
 ?>
