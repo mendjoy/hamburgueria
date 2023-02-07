c40f13af713c663ac2835f87d8f7186e7dcd1b92
joyce <joycemendes.jm79@gmail.com>
2023-02-06 21:53:54 -0300

    orders

--- process/orders.php
+++ process/orders.php
@@ -2,4 +2,50 @@
 
     include_once("conn.php");
 
+    $method = $_SERVER["REQUEST_METHOD"];
+
+    if($method === "GET") {
+
+        $pedidosQuery = $conn->query("SELECT * FROM pedidos;");
+
+        $pedidos = $pedidosQuery->fetchAll();
+
+        $hamburguers = [];
+
+        foreach($pedidos as $pedido){
+
+            $hamburguer = [];
+
+            //Resgatando hamburguer
+            $hamburguer["id"] = $pedido["hamburguer_id"];
+
+            $hamburguerQuery = $conn->prepare("SELECT * FROM  hamburguers WHERE id = :hamburguer_id");
+
+            $hamburguerQuery->bindParam(":hamburguer_id", $hamburguer["id"]);
+
+            $hamburguerQuery->execute();
+
+            $hamburguerData = $hamburguerQuery->fetch(PDO::FETCH_ASSOC);
+
+
+            //Resgatando o tipo de pão
+            $paoQuery = $conn->prepare("SELECT * FROM  pao WHERE id = :pao_id");
+
+            $paoQuery->bindParam(":pao_id", $hamburguerData["pao_id"]);
+
+            $paoQuery->execute();
+
+            $pao = $paoQuery->fetch(PDO::FETCH_ASSOC);
+
+            $hamburguer["pao"] = $pao["tipo"];
+
+
+
+
+        };
+
+    } else if ($method === "POST"){
+
+    }
+
 ?>
